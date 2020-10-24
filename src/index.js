4fd5089bdda1405b8981c907633ae167a5e92a44
MarinaPorto <marina.x.sea@gmail.com>
2020-10-24 18:43:40 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    if (n < 0) {
+        n = n * (-1);
+    }
+    let result = 0;
+    while (n) {
+        result = result * 10 + n % 10;
+        n = Math.floor(n / 10);
+    }
+    return result;
 }
